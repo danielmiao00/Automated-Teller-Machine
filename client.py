9f1060948c65501fcd91226787ec1f4ea802ab92
danielmiao00 <61894311+danielmiao00@users.noreply.github.com>
2021-11-11 16:33:56 -0600

    Functional server.py and client.py

---
+++ client.py
@@ -0,0 +1,51 @@
+import socket
+
+
+# Menu method
+def menu():
+    print("----Welcome to Bank's Banks----")
+    print("Press 1 for Deposit.")
+    print("Press 2 for Withdrawal")
+    print("Press 3 for Account Balance")
+    print("Press 4 to Quit")
+
+
+host = socket.gethostname()  # Local machine name
+port = 12345  # Reserving port
+s = socket.socket()  # Socket object
+s.connect((host, port))  # Bind to port
+
+# Constantly prompt until ended
+while True:
+    menu()  # Calling menu method to display menu to the user
+    print("What would you like to do: ")
+    user_input = input()  # Input for service wanted
+
+    # Check that if the input deposit or withdrawal
+    if user_input == '1' or user_input == '2':
+        msg = str.encode(str(user_input), 'utf-8')
+        s.send(msg)
+        print(s.recv(1024))
+        user_input = input()  # Input for dollar amount
+        msg = str.encode(str(user_input), 'utf-8')
+        s.send(msg)
+        print(s.recv(1024))
+
+    # Check for balance
+    elif user_input == '3':
+        msg = str.encode(str(user_input), 'utf-8')
+        s.send(msg)
+        print(s.recv(1024))
+
+    # Check for exit
+    elif user_input == '4':
+        msg = str.encode(str(user_input), 'utf-8')
+        s.send(msg)
+        print(s.recv(1024))
+        break
+
+    # For not expected values
+    else:
+        print("Incorrect input, please try again")
+
+s.close  # Close the socket
---
+++ server.py
@@ -0,0 +1,128 @@
+import socket
+import sys
+
+
+class Account:
+
+    def __init__(self):
+        # Initialize our account and set a temp variable
+        self.account_balance = int(100)
+        self.temp_balance = self.account_balance
+
+    # Withdraw method
+    def withdraw(self, amount, client):
+        self.account_balance -= amount  # Adjust account balance
+
+        # Error checking for overdraft
+        if self.account_balance < 0:
+
+            self.account_balance = self.temp_balance  # Reset account value if overdraft
+            # For server output
+            print("Sorry, attempting to withdraw $%.1f when you only have $%.1f" % (amount, self.temp_balance))
+            # For client output
+            confirm = "Sorry, attempting to withdraw $%.1f when you only have $%.1f" % (amount, self.temp_balance)
+            client.send(confirm.encode())
+        else:
+            # Confirmation
+            self.temp_balance = self.account_balance  # Adjust account balance
+            self.temp_balance = self.account_balance  # Adjust temp variable
+            print("Withdrew $%.1f successfully" % amount)  # For server output
+            confirm = "Withdrew $%.1f successfully" % amount  # For client output
+            client.send(confirm.encode())
+
+    # Deposit method
+    def deposit(self, amount, client):
+        self.account_balance += amount  # Adjust account balance
+        self.temp_balance = self.account_balance  # Adjust temp variable
+
+        # Confirmation
+        print("Deposited $%.1f successfully" % amount)  # For server output
+        confirm = "Deposited $%.1f successfully" % amount  # For client output
+        client.send(confirm.encode())
+
+    # Balance check method
+    def balance(self, client):
+        print("Remaining balance is $%.1f" % self.account_balance)  # For server output
+        confirm = "Remaining balance is $%.1f" % self.account_balance  # For client output
+        client.send(confirm.encode())
+
+    # Error message method
+    def incorrect(self, client):
+        print("Sorry, detected an invalid input")  # For server output
+        confirm = "Sorry, detected an invalid input"  # For client output
+        client.send(confirm.encode())
+
+
+class Server:
+    host = socket.gethostname()  # Acquire machine name
+    port = 12345  # Reserving port
+    s = socket.socket()  # Socket object
+    s.bind((host, port))  # Bind to port
+
+    s.listen(5)  # Waiting for the client
+    client, address = s.accept()  # Create client connection
+    print('Got connection from', address)
+    server_obj = Account()
+
+    while True:
+        user_input = client.recv(1024).decode()
+
+        # Deposit Function
+        if user_input == '1':
+            # Prompting user for desired deposit amount
+            client.send("How much would you like to deposit?".encode())
+
+            # Error Handling for floats
+            try:
+                amount = int(client.recv(1024).decode())
+            except ValueError:
+                # Send error message to user
+                server_obj.incorrect(client)
+                continue
+
+            # Error Handling for negative numbers
+            if amount > 0:
+                # Depositing money to the account
+                server_obj.deposit(amount, client)
+            else:
+                # Send error message to user
+                server_obj.incorrect(client)
+
+        # Withdraw Function
+        elif user_input == '2':
+            # Prompting user for desired withdrawal amount
+            client.send("How much would you like to withdraw?".encode())
+
+            # Error Handling for floats
+            try:
+                amount = int(client.recv(1024).decode())
+            except ValueError:
+                # Send error message to user
+                server_obj.incorrect(client)
+                continue
+
+            # Error Handling for negative numbers
+            if amount > 0:
+                # Withdrawing money from account
+                server_obj.withdraw(amount, client)
+            else:
+                # Send error message to user
+                server_obj.incorrect(client)
+
+        # Check balance function
+        elif user_input == '3':
+            # Displaying the current balance
+            server_obj.balance(client)
+
+        # Close server connection function
+        elif user_input == '4':
+            # Exiting
+            client.send("Thank you choosing Bank's Banks".encode())
+            sys.exit(1)
+
+        # Check for unexpected values (redundant, but failsafe)
+        else:
+            print("Incorrect input, please try again")
+            client.send("Incorrect input, please try again".encode())
+
+    client.close()  # Close the connection
